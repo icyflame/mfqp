a16e77ce9117dadfde90b081843cfa4d1882e38d
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-09-16 22:21:42 +0530

    add gulp file - minifies json and copies everything else

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var minifyJSON = require('gulp-jsonminify');
+
+var fs = require('fs');
+
+if (!fs.existsSync('dist')) {
+  fs.mkdirSync('dist');
+}
+
+gulp.task('json', function () {
+  var subjects_data_str = fs.readFileSync('./data/data.json').toString();
+  try {
+    var subjects_data_obj = JSON.parse(subjects_data_str);
+  } catch (err) {
+    console.log("Unable to parse data/data.json. Invalid JSON");
+    console.error(err);
+    process.exit(1);
+  }
+  return fs.writeFileSync('./dist/data.json', JSON.stringify(subjects_data_obj));
+});
+
+gulp.task('copy', function () {
+  return gulp.src(['index.html', 'resources/**/*'])
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', [ 'json', 'copy' ]);
